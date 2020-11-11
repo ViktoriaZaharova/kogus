8fe3c8000922da3c838e9fc10a47c2c63a32529b
viktoriazaharova <zaharova181114@gmail.com>
2020-11-11 18:37:51 +0200

    edit

--- app/js/common.js
+++ app/js/common.js
@@ -6,14 +6,59 @@ $('.main-slider').slick({
 
 $('.special-slider').slick({
     slidesToShow: 3,
-    appendArrows: '.special-slider__nav'
+    appendArrows: '.special-slider__nav',
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
 $('.new-products-slider').slick({
     slidesToShow: 3,
-    appendArrows: '.new-products-slider__nav'
+    appendArrows: '.new-products-slider__nav',
+    responsive: [
+        {
+            breakpoint: 1100,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 900,
+            settings: {
+                slidesToShow: 3,
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
 });
 
 $('.menu-category__item').click(function () {
    $(this).find('.menu-category-submenu').slideToggle();
+});
+
+$('.btn-burger').click(function () {
+   $(this).toggleClass('click');
+   $('.mobile-menu').fadeToggle();
 });
